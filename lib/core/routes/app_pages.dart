a50abdc5e4cfeaafd97ad6598d36c885999b4d78
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-12-28 11:43:47 +0600

    route config for go_router

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/pages/sign_in_page.dart';
 import '../../features/chat/presentation/pages/users_list_page.dart';
 import '../../features/chat/presentation/pages/chat_page.dart';
@@ -8,21 +9,29 @@ class AppPages {
   // Prevent instantiation
   AppPages._();
 
-  static final routes = [
-    GetPage(
-      name: AppRoutes.signIn,
-      page: () => const SignInPage(),
-    ),
-    GetPage(
-      name: AppRoutes.usersList,
-      page: () => const UsersListPage(),
-    ),
-    GetPage(
-      name: AppRoutes.chat,
-      page: () {
-        final userId = Get.arguments as String? ?? Get.parameters['userId'] ?? '';
-        return ChatPage(userId: userId);
-      },
-    ),
-  ];
+  static final GoRouter router = GoRouter(
+    initialLocation: AppRoutes.signIn,
+
+    routes: [
+      GoRoute(
+        path: AppRoutes.signIn,
+        name: AppRoutes.signInName,
+        builder: (context, state) => SignInPage(),
+      ),
+      GoRoute(
+        path: AppRoutes.usersList,
+        name: AppRoutes.usersListName,
+        builder: (context, state) => UsersListPage(),
+      ),
+      GoRoute(
+        path: AppRoutes.chat,
+        name: AppRoutes.chatName,
+        builder: (context, state) {
+          final userId = state.pathParameters['userId'] ?? '';
+          return ChatPage(userId: userId);
+        },
+      ),
+    ],
+    errorBuilder: (context, state) => SignInPage(),
+  );
 }
