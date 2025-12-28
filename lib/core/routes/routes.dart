833fa54ebe4c22364047d956ea066f9bdf2427b6
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-12-28 11:43:14 +0600

    modified to align with go_router package

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,11 +1,16 @@
 class AppRoutes {
   // Prevent instantiation
   AppRoutes._();
-
+  
   // Auth Routes
   static const String signIn = '/';
   
   // Chat Routes
   static const String usersList = '/users-list';
-  static const String chat = '/chat';
+  static const String chat = '/chat/:userId';
+  
+  // Route names for named navigation
+  static const String signInName = 'signIn';
+  static const String usersListName = 'usersList';
+  static const String chatName = 'chat';
 }
